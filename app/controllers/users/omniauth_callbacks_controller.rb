9796da2b12739dd143937c2bf6f56a12537481e7
Egor Rybchenko <macmaczhl@gmail.com>
2017-01-21 02:51:01 +0300

    fix: Authorize message Remove message about success authorization

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,6 @@ module Users
 
       if @user.persisted?
         sign_in @user
-        set_flash_message(:notice, :success, kind: FLASH_MESSAGE_KIND)
         redirect_to tweets_path(@user)
       else
         set_flash_message(:notice, :failure, kind: FLASH_MESSAGE_KIND, reason: @user.errors.full_messages.first)
