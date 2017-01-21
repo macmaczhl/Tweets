8d466c9d6e43cfba0e1f70f22a204ca85895ddec
Egor Rybchenko <macmaczhl@gmail.com>
2017-01-22 01:51:16 +0300

    refactor: Omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.config.twitter_key, Rails.application.config.twitter_secret,
-    {
-      authorize_params: {
-        force_login: 'true'
-      }
-    }
+  provider :twitter,
+           Rails.application.config.twitter_key,
+           Rails.application.config.twitter_secret,
+           authorize_params: {
+             force_login: 'true'
+           }
 end
