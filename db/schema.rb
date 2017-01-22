ffa347c1d02150fce4171ce2b3f33fdf1900cf4b
Egor Rybchenko <macmaczhl@gmail.com>
2017-01-23 00:12:04 +0300

    fix: Tweets text Change tweets text type to string BREAKING CHANGE: migration deleted. Need db:drop

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122204619) do
+ActiveRecord::Schema.define(version: 20170122192342) do
 
   create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "text",       limit: 65535
+    t.string   "text"
     t.string   "image"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "user_id",                  null: false
-    t.string   "url",                      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id",    null: false
+    t.string   "url",        null: false
     t.index ["user_id"], name: "index_tweets_on_user_id", using: :btree
   end
 
